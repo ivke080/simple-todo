f3a9cced64934394e86c7e661ae147b787f42958
ivke080 <ivke11080@gmail.com>
2016-03-05 22:10:34 +0100

    change date display

--- show_posts.php
+++ show_posts.php
@@ -8,11 +8,12 @@
         exit();
     }
     while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+        $date = date("d-m-Y", strtotime($row[2])); // changing from mysqls year-month-date to date-month-year
         echo '<article class="todo '.$row[0].'">
             <div class="row">
                 <div class="col-xs-12 article-content">'.$row[1].'</div></div>
                 <div class="row">
-                    <div class="col-xs-12 text-center date">'.$row[2].'</div>
+                    <div class="col-xs-12 text-center date">'.$date.'</div>
                     </div>
         </article>';
     }
